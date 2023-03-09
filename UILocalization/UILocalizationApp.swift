115a5ea879dd45bced7d372dc5b9fcd08fc483ce
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-03-08 23:29:05 -0300

    Initial Commit

---
+++ UILocalization/UILocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  UILocalizationApp.swift
+//  UILocalization
+//
+//  Created by Luiz Araujo on 08/03/23.
+//
+
+import SwiftUI
+
+@main
+struct UILocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
